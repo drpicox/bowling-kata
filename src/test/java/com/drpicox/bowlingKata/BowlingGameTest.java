7acd36a70a5bb24afc0ef52156b73961b8b00fef
David Rodenas <david.rodenas@gmail.com>
2018-05-04 08:17:16 +0200

    commit 0

---
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -0,0 +1,15 @@
+package com.drpicox.bowlingKata;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static com.google.common.truth.Truth.assertThat;
+
+public class BowlingGameTest {
+
+//    @Test
+//    public void test_example() {
+//        var n = 3 + 2 / 2 - 1;
+//        assertThat(n).isEqualTo(3);
+//    }
+
+}
